a9cf330d08042efc99b1e85635f4b67bfd5f98bf
Rizal Fadlullah <rizal.fadlullah@gmail.com>
2023-11-11 11:27:13 +0700

    installing package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import 'bootstrap';
 
 createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
