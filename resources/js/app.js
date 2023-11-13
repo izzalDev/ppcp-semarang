5e468231cfdc3bf76f5bef706f4984a7fb60f86d
Rizal Fadlullah <rizal.fadlullah@gmail.com>
2023-11-13 12:40:56 +0700

    Fix bug merge conflict

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,6 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m.js'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
